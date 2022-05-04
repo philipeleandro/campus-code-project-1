2e39ae3650e118acad2cfeb3759329ac79d47a0f
Philipe <philipe-leandro@hotmail.com>
2022-05-04 11:58:28 -0300

    Register new warehouse

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,15 @@ class WarehousesController < ApplicationController
     id = params[:id]
     @warehouse = Warehouse.find(id)
   end
+
+  def new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :description, :code, :address, :city, :cep, :area)
+    @warehouse = Warehouse.new(warehouse_params)
+    @warehouse.save
+    
+    redirect_to root_path, notice: 'Cadastrado com sucesso'
+  end
 end
