f5831dd288635ee1a964dc3781bdbe6a6181f964
Philipe <philipe-leandro@hotmail.com>
2022-05-08 09:52:05 -0300

    Refactor warehouse's controller and render form

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,7 +1,7 @@
 class WarehousesController < ApplicationController
+  before_action :set_warehouse, only: [:show, :edit, :update]
+
   def show
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
   end
 
   def new
@@ -9,7 +9,6 @@ class WarehousesController < ApplicationController
   end
 
   def create
-    warehouse_params = params.require(:warehouse).permit(:name, :description, :code, :address, :city, :cep, :area)
     @warehouse = Warehouse.new(warehouse_params)
     
     if @warehouse.save
@@ -21,20 +20,23 @@ class WarehousesController < ApplicationController
   end
 
   def edit
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
   end
 
   def update
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-    warehouse_params = params.require(:warehouse).permit(:name, :description, :code, :address, :city, :cep, :area)
-
     if @warehouse.update(warehouse_params)
       redirect_to warehouse_path(@warehouse), notice: 'Galpão atualizado com sucesso'
     else
       flash.now[:notice] = 'Não foi possível atualizar o galpão'
       render 'edit'
     end
+  end
+
+  private
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :description, :code, :address, :city, :cep, :area)
+  end
+
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:id])
   end
 end
