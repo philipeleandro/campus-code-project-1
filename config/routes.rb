24f9d87f495fa4231250d4151b59cb590cbc4d28
Philipe <philipe-leandro@hotmail.com>
2022-05-11 11:21:45 -0300

    Create model and index action to product models

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :suppliers, only: [:index, :new, :create, :show, :edit, :update]
+  resources :product_models, only: [:index]
 end
---
+++ spec/system/user_views_products_models_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'User sees product models' do
+  it 'in menu' do
+    visit root_path
+    within('nav') do
+      click_on 'Modelos de produtos'
+    end
+
+    expect(current_path).to eq product_models_path
+  end
+
+  it 'success' do
+    supplier = Supplier.create!(corporate_name: 'Samsung LTDA', brand_name: 'Samsung', registration_number: '1234567897894', full_address: 'Rua teste, 1000', city: 'São Paulo', state: 'São Paulo', email: 'example@gmail.com', phone: '11254637894')
+
+    ProductModel.create!(name: 'TV 32', weigth: 8000, width: 70,height: 45, depth: 10, sku: 'TV32-SAMSU-XPT090', supplier: supplier)
+    ProductModel.create!(name: 'Notebook', weigth: 2500, width: 35,height: 23, depth: 20, sku: 'NBI3-SAMSU-LUX425', supplier: supplier)
+
+    visit root_path
+    within('nav') do
+      click_on 'Modelos de produtos'
+    end
+
+    expect(page).to have_content('TV 32')
+    expect(page).to have_content('Notebook')
+    expect(page).to have_content('Samsung')
+    expect(page).to have_content('Samsung')
+    expect(page).to have_content('TV32-SAMSU-XPT090')
+    expect(page).to have_content('NBI3-SAMSU-LUX425')
+  end
+
+  it 'does not exist register product model' do
+    visit root_path
+    click_on 'Modelos de produtos'
+
+    expect(page).to have_content('Não há modelo de produto cadastrado')
+  end
+end
